7d4f1773577ed3ce4fde79fdc25db9c70deb2d02
Mazen Mahmoud <mazenmahmoud1237@gmail.com>
2026-09-10 20:50:23 +0300

    Delete risc_v_single_cycle_processor directory

--- risc_v_single_cycle_processor/program/program_control.s
+++
@@ -1,7 +0,0 @@
-# Test Branching (BEQ)
-addi x1, x0, 5     # x1 = 5
-addi x2, x0, 5     # x2 = 5
-beq  x1, x2, label # Branch taken because x1 == x2
-addi x3, x0, 99    # Should be skipped
-label:
-addi x4, x0, 1     # x4 = 1 (Branch taken verified)
